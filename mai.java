0212adb942495706e5c6feb0961c5505156b40c8
Kanishk Dendukuri <65775557+Kanishk-Dendukuri@users.noreply.github.com>
2024-06-05 10:27:33 -0700

    Update and rename main.java to mai.java

--- mai.java
+++ mai.java
@@ -1,17 +1,17 @@
-class main{
+class mai{
   
   
-  public static void main(String args[]){
+  public static void main(String args[]) {
     System.out.println("HI");
   }
 
   
-  int car(){
+  int car() {
     return 0;
   }
 
   
-  int bike(){
+  int bike() {
     return 1;
   }
 }
